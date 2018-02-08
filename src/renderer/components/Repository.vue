8cc01ee00400b55070584bb4e7d4d43379381f23
Unknown <sean111@gmail.com>
2018-02-08 15:35:09 -0500

    Clean up some unused code in Repository.vue

--- src/renderer/components/Repository.vue
+++ src/renderer/components/Repository.vue
@@ -1,17 +1,5 @@
 <template>
   <section class="section">
-    <!--<ul>-->
-      <!--<li v-for="deployment in deployments">-->
-        <!--<h5>-->
-          <!--<i v-if="deployment.state === 'success'" class="text-success">S</i>-->
-          <!--<i class="text-danger" v-else>E</i>-->
-          <!--{{ deployment.deployed_at | formatDate }} by {{ deployment.author_name }}-->
-        <!--</h5>-->
-        <!--<p>-->
-          <!--{{ deployment.comment }}-->
-        <!--</p>-->
-      <!--</li>-->
-    <!--</ul>-->
     <b-card v-for="deployment in deployments" :key="deployment.id" header-text-variant="white" :header="header(deployment)" :header-bg-variant="cardClass(deployment)" :border-variant="cardClass(deployment)" style="margin-top: 10px">
       <p class="card-text">{{ deployment.comment }}</p>
     </b-card>
@@ -32,10 +20,6 @@
         this.data = null
         this.$Progress.start()
         this.$electron.ipcRenderer.send('get-deployments', {'repository_id': this.id})
-        // this.$electron.ipcRenderer.on('get-deployments-response', (event, data) => {
-        //   this.deployments = data.entries
-        //   this.$Progress.start()
-        // })
       }
     },
     mounted () {
